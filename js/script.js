75a66b13945487c6a0676169320e6f664d1701ff
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-27 17:19:54 +0200

    miss "this" per cambiare colore allo square

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function generateGrid (gridX, gridY, container){
+container.style.width = `calc(50px * ${gridX} + 25px)`;
+container.innerHTML = ""
+for (let i = 1; i <= (gridX * gridY); i++){
+ container.innerHTML += `<div class="square">${i}</div>`
+}
+}
+
+const squareContainer = document.getElementById("squarecontainer");
+const playButton = document.getElementById("playbtn");
+let difficulty = document.getElementById("difficultySelect");
+
+playButton.addEventListener("click", function() {
+
+console.log (difficulty.value)
+
+if (difficulty.value === "easy"){
+ grid = generateGrid(10, 10, squareContainer);
+}
+
+if (difficulty.value === "medium"){
+ grid = generateGrid(9, 9, squareContainer);
+}
+
+if (difficulty.value === "hard"){
+ grid = generateGrid(7, 7, squareContainer);
+}
+})
+
+if (this.squareContainer){
+ this.squareContainer.classList.add("clicked");
+}
+
